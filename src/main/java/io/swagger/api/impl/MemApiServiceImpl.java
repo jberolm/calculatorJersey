a32de21a6f6cdef44261687ade153db86e71c198
Javier Berrocal <jberolm@unex.es>
2018-11-02 16:24:08 +0000

    Added metric mem service

--- src/main/java/io/swagger/api/impl/MemApiServiceImpl.java
+++ src/main/java/io/swagger/api/impl/MemApiServiceImpl.java
@@ -17,6 +17,9 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.core.SecurityContext;
 
 import javax.validation.constraints.*;
+import java.io.DataOutputStream;
+import java.net.Socket;
+import java.util.Random;
 
 @javax.annotation.Generated(value = "io.swagger.codegen.languages.java.JavaJerseyServerCodegen", date = "2018-10-24T19:54:08.502Z[GMT]")
 
@@ -25,6 +28,18 @@ public class MemApiServiceImpl extends MemApiService {
     @Override
     public Response storeData( @NotNull Integer value, SecurityContext securityContext) throws NotFoundException {
         System.out.println("Storing the value:" + value);
+        //Sending metric
+        try{
+            Socket conn = new Socket("538fc7e2.carbon.hostedgraphite.com", 2003);
+            DataOutputStream dos = new DataOutputStream(conn.getOutputStream());
+            Random rand = new Random();
+            
+            dos.writeBytes("ca123819-c3d8-4134-80a3-0b568c23ca80.ist-calc.mem.timeExecution"+rand.nextFloat()+"\n");
+            conn.close();
+        }catch (Exception e){
+            System.out.println("ERROR_METRIC: " + e);
+        }
+        
         return Response.ok().entity(true).build();
     }
     
